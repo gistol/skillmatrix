fa3acb30cc56bda8dfaafc6c0bd0a5cd722d1941
demijohn <mbilcik123@gmail.com>
2019-02-26 15:03:01 +0100

    Add entity relations

--- src/Entity/Person.php
+++ src/Entity/Person.php
@@ -2,6 +2,8 @@
 
 namespace App\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -21,6 +23,16 @@ class Person
      */
     private $name;
 
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Rating", mappedBy="person")
+     */
+    private $ratings;
+
+    public function __construct()
+    {
+        $this->ratings = new ArrayCollection();
+    }
+
     public function getId(): ?int
     {
         return $this->id;
@@ -34,6 +46,37 @@ class Person
     public function setName(string $name): self
     {
         $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Rating[]
+     */
+    public function getRatings(): Collection
+    {
+        return $this->ratings;
+    }
+
+    public function addRating(Rating $rating): self
+    {
+        if (!$this->ratings->contains($rating)) {
+            $this->ratings[] = $rating;
+            $rating->setPerson($this);
+        }
+
+        return $this;
+    }
+
+    public function removeRating(Rating $rating): self
+    {
+        if ($this->ratings->contains($rating)) {
+            $this->ratings->removeElement($rating);
+            // set the owning side to null (unless already changed)
+            if ($rating->getPerson() === $this) {
+                $rating->setPerson(null);
+            }
+        }
 
         return $this;
     }
--- src/Entity/Rating.php
+++ src/Entity/Rating.php
@@ -26,6 +26,23 @@ class Rating
      */
     private $note;
 
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Reviewer")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $reviewer;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Skill")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $skill;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Person", inversedBy="ratings")
+     */
+    private $person;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -51,6 +68,42 @@ class Rating
     public function setNote(?string $note): self
     {
         $this->note = $note;
+
+        return $this;
+    }
+
+    public function getReviewer(): ?Reviewer
+    {
+        return $this->reviewer;
+    }
+
+    public function setReviewer(?Reviewer $reviewer): self
+    {
+        $this->reviewer = $reviewer;
+
+        return $this;
+    }
+
+    public function getSkill(): ?Skill
+    {
+        return $this->skill;
+    }
+
+    public function setSkill(?Skill $skill): self
+    {
+        $this->skill = $skill;
+
+        return $this;
+    }
+
+    public function getPerson(): ?Person
+    {
+        return $this->person;
+    }
+
+    public function setPerson(?Person $person): self
+    {
+        $this->person = $person;
 
         return $this;
     }
