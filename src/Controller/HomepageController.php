da41be48f2f6caacca143733d2572f35644efeaa
demijohn <mbilcik123@gmail.com>
2019-02-27 10:06:50 +0100

    Add initial HomepageController

---
+++ src/Controller/HomepageController.php
@@ -0,0 +1,33 @@
+<?php declare(strict_types = 1);
+
+namespace App\Controller;
+
+use App\Repository\PersonRepository;
+use Symfony\Component\HttpFoundation\Response;
+
+class HomepageController
+{
+    /**
+     * @var \App\Repository\PersonRepository
+     */
+    private $personRepository;
+
+    public function __construct(PersonRepository $personRepository)
+    {
+        $this->personRepository = $personRepository;
+    }
+
+    public function index()
+    {
+        $persons = $this->personRepository->findAll();
+
+        foreach ($persons as $person) {
+            $ratings = $person->getRatings();
+            foreach ($ratings as $rating) {
+                dump($rating);
+            }
+        }
+
+        return new Response('OK');
+    }
+}
