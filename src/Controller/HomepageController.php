534ce9fdcb0162ebc4a1bd950837055db72d3432
demijohn <mbilcik123@gmail.com>
2019-03-01 14:25:34 +0100

    Modify templates to load Vue app

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -2,28 +2,13 @@
 
 namespace App\Controller;
 
-use App\Service\SkillmatrixService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
-class HomepageController
+class HomepageController extends AbstractController
 {
-    /**
-     * @var \App\Service\SkillmatrixService
-     */
-    private $skillmatrixService;
-
-    public function __construct(SkillmatrixService $skillmatrixService)
-    {
-        $this->skillmatrixService = $skillmatrixService;
-    }
-
     public function index(): Response
     {
-        $skillmatrix = $this->skillmatrixService->get();
-
-        dump(json_encode($skillmatrix));
-
-        dd($skillmatrix);
-        return new Response('OK');
+        return $this->render('homepage/index.html.twig');
     }
 }
