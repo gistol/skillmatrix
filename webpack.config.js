7658473e2fb00e2c8a651613b902976a14ad969b
demijohn <mbilcik123@gmail.com>
2019-03-01 13:36:06 +0100

    Install Vue
    yarn add --dev vue vue-loader vue-template-compiler

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,8 @@ Encore
      * Each entry will result in one JavaScript file (e.g. app.js)
      * and one CSS file (e.g. app.css) if you JavaScript imports CSS.
      */
-    .addEntry('app', './assets/js/app.js')
+    .addEntry('app', './assets/vue/index.js')
+    //.addEntry('app', './assets/js/app.js')
     //.addEntry('page1', './assets/js/page1.js')
     //.addEntry('page2', './assets/js/page2.js')
 
@@ -26,7 +27,8 @@ Encore
 
     // will require an extra script tag for runtime.js
     // but, you probably want this, unless you're building a single-page app
-    .enableSingleRuntimeChunk()
+    //.enableSingleRuntimeChunk()
+    .disableSingleRuntimeChunk()
 
     /*
      * FEATURE CONFIG
@@ -40,6 +42,7 @@ Encore
     .enableSourceMaps(!Encore.isProduction())
     // enables hashed filenames (e.g. app.abc123.css)
     .enableVersioning(Encore.isProduction())
+    .enableVueLoader()
 
     // enables Sass/SCSS support
     //.enableSassLoader()
